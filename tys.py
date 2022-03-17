4ac13d4971010958c8c24f7871107aed818a6f72
Rohit Sahoo <47326210+rockyhandso@users.noreply.github.com>
2022-03-17 16:45:11 +0530

    Add files via upload

---
+++ tys.py
@@ -0,0 +1,27 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input("Enter number:"))
+besttemp = 40
+
+for i in input().split():
+    t = int(i)
+
+    # Write an answer using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    print(t, file=sys.stderr, flush=True)
+
+    if abs(t) < abs(besttemp):
+        besttemp = t
+    if abs(t) == abs(besttemp) and t > 0:
+        besttemp = t
+
+if besttemp == 40:
+    c = besttemp - besttemp
+    print(c)
+else:
+    print("try later")
+print(c)
